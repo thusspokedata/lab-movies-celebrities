df40b33e4c3f20ed7511500c88be84dfb369e89c
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2022-11-13 09:13:10 +0100

    iteration 6 & 7

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,3 +1,48 @@
 const router = require('express').Router();
+const Celebrity = require('../models/Celebrity.model');
+const Movie = require('../models/Movie.model');
+
+router.get('/create', (req, res) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      console.log('this is celebrities!!!!!!', celebrities);
+      res.render('movies/new-movie', celebrities);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render('movies/new-movie');
+    });
+});
+
+router.post('/create', (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  Movie.create({
+    title,
+    genre,
+    plot,
+    cast,
+  })
+    .then((movies) => {
+      console.log(movies);
+      res.redirect('/movies');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render('movies/new-movie');
+    });
+});
+
+router.get('/', (req, res, next) => {
+  Movie.find()
+    .populate('cast')
+    .then((movies) => {
+      console.log(movies);
+      res.render('movies/movies', movies);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render('movies/new-movie');
+    });
+});
 
 module.exports = router;
